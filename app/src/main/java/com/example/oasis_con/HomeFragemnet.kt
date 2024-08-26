75f509c696995166fae7ee14b8e698cd0a3e58bd
hodu26 <gjdhks1212@gmail.com>
2024-08-26 23:54:50 +0900

    eighth

--- app/src/main/java/com/example/oasis_con/HomeFragemnet.kt
+++ app/src/main/java/com/example/oasis_con/HomeFragemnet.kt
@@ -25,13 +25,10 @@ class HomeFragment : Fragment() {
         //val chatButton: Button = view.findViewById(R.id.chatButton)
         //val mapButton: Button = view.findViewById(R.id.mapButton)
         val gameButton: Button = view.findViewById(R.id.gameButton)
-<<<<<<< HEAD
-        val apiButton: Button = view.findViewById(R.id.ApiButton)
-        val infoButton: Button = view.findViewById(R.id.InfoButton)
-        val mypageButton: Button = view.findViewById(R.id.MypageButton)
-=======
+//        val apiButton: Button = view.findViewById(R.id.ApiButton)
+//        val infoButton: Button = view.findViewById(R.id.InfoButton)
+//        val mypageButton: Button = view.findViewById(R.id.MypageButton)
         //val apiButton: Button = view.findViewById(R.id.ApiButton)
->>>>>>> 0c28255ea8fa03b9fb87479bb6d7451bc54d9677
 
         // 각 버튼에 클릭 리스너 설정
         //logoutButton.setOnClickListener { onLogoutClicked() }
@@ -39,65 +36,61 @@ class HomeFragment : Fragment() {
         //chatButton.setOnClickListener { onChatClicked() }
         //mapButton.setOnClickListener { onMapClicked() }
         gameButton.setOnClickListener { onGameClicked() }
-<<<<<<< HEAD
-        apiButton.setOnClickListener { onApiClicked() }
-        infoButton.setOnClickListener { onInfoClicked() }
-        mypageButton.setOnClickListener { onMypageClicked() }
-=======
+//        apiButton.setOnClickListener { onApiClicked() }
+//        infoButton.setOnClickListener { onInfoClicked() }
+//        mypageButton.setOnClickListener { onMypageClicked() }
+
         //apiButton.setOnClickListener { onApiClicked() }
->>>>>>> 0c28255ea8fa03b9fb87479bb6d7451bc54d9677
 
         return view
     }
 
-    private fun onLogoutClicked() {
-        // 로그아웃 로직 구현
-        mAuth.signOut()
-        val intent = Intent(activity, MainActivity::class.java)
-        startActivity(intent)
-//        finish()
-    }
-/*
-    private fun onBoardClicked() {
-        // BoardActivity로 전환
-        val intent = Intent(activity, BoardActivity::class.java)
-        startActivity(intent)
-    }
+//    private fun onLogoutClicked() {
+//        // 로그아웃 로직 구현
+//        mAuth.signOut()
+//        val intent = Intent(activity, MainActivity::class.java)
+//        startActivity(intent)
+//    }
+//
+//    private fun onBoardClicked() {
+//        // BoardActivity로 전환
+//        val intent = Intent(activity, BoardActivity::class.java)
+//        startActivity(intent)
+//    }
+//
+//    private fun onChatClicked() {
+//        // 채팅 화면으로 이동하는 로직 구현
+//        Toast.makeText(context, "Chat clicked", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    private fun onMapClicked() {
+//        // 지도 화면으로 이동하는 로직 구현
+//        Toast.makeText(context, "Map clicked", Toast.LENGTH_SHORT).show()
+//    }
 
-    private fun onChatClicked() {
-        // 채팅 화면으로 이동하는 로직 구현
-        Toast.makeText(context, "Chat clicked", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun onMapClicked() {
-        // 지도 화면으로 이동하는 로직 구현
-        Toast.makeText(context, "Map clicked", Toast.LENGTH_SHORT).show()
-    }
-*/
     private fun onGameClicked() {
         // 게임 화면으로 이동하는 로직 구현
         val intent = Intent(activity, GameActivity::class.java)
         startActivity(intent)
     }
-/*
-    private fun onApiClicked() {
-        // API 관련 기능을 실행하는 로직 구현
-<<<<<<< HEAD
-        Toast.makeText(context, "API clicked", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun onInfoClicked() {
-        val intent = Intent(activity, InfoActivity::class.java)
-        startActivity(intent)
-    }
-
-    private fun onMypageClicked() {
-        val intent = Intent(activity, MyPageActivity::class.java)
-        startActivity(intent)
-    }
-=======
-        val intent = Intent(activity, ItemActivity::class.java)
-        startActivity(intent)
-    }*/
->>>>>>> 0c28255ea8fa03b9fb87479bb6d7451bc54d9677
+//
+//    private fun onApiClicked() {
+//        // API 관련 기능을 실행하는 로직 구현
+//        Toast.makeText(context, "API clicked", Toast.LENGTH_SHORT).show()
+//    }
+//
+//    private fun onInfoClicked() {
+//        val intent = Intent(activity, InfoActivity::class.java)
+//        startActivity(intent)
+//    }
+//
+//    private fun onMypageClicked() {
+//        val intent = Intent(activity, MyPageActivity::class.java)
+//        startActivity(intent)
+//    }
+//
+//    private fun on
+//        val intent = Intent(activity, ItemActivity::class.java)
+//        startActivity(intent)
+//    }
 }
